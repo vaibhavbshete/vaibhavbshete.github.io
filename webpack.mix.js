b1ce6d49a2665b7542beccd495bc3d1a4fe3d180
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2022-04-12 18:10:27 +0530

    run through laravel-mix and add css versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,14 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
-mix.disableNotifications();
+mix.disableNotifications().setPublicPath('.').version();
 
-mix.css('src/main.css', 'pub/main.css', require('tailwindcss'));
+mix.css('src/main.css', 'pub/main.css');
+
+mix.browserSync({
+    
+    server: { baseDir: '.', index: 'index.html' },
+    files:[
+       './index.html'
+     ],
+ })
