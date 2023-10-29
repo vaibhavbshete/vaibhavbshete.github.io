d1785bea7086df9f56040dab99a8a0bd0895e397
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2023-10-30 00:17:47 +0530

    revert directory structure and use readymade gh pages action in workflow - makes things simple again - follows vite documentation closely

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 export default defineConfig({
-    root: './src',
     build: {
         rollupOptions: {
             input: {
-                main: resolve (__dirname, 'src', 'index.html'),
-                err404: resolve (__dirname, 'src', '404.html'),
+                main: resolve (__dirname, 'index.html'),
+                err404: resolve (__dirname, '404.html'),
             }
         },
-        outDir: '../',
-        emptyOutDir: false,
     }
 })
