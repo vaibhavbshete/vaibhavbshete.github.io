627906e14f945193df1596b697d5465afebb1924
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2023-11-14 18:42:24 +0530

    shift tab definition to main.js - use template to display tabs

--- src/main.js
+++ src/main.js
@@ -2,4 +2,32 @@ import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
+
+Alpine.data('tabs', () => {
+    return {
+        tabs: [
+            {
+                id:'dev',
+                name: 'PHP Web Developer',
+            },
+            {
+                id: 'editor',
+                name: 'Film Editor',
+            },
+            {
+                id: 'colourist',
+                name: 'Colourist',
+            },
+            {
+                id: 'designer',
+                name: 'Graphic Designer',
+            }
+        ],
+        openTab: 'dev',
+        tabWorks: (role) => {
+            return works.filter(work => (new Set(work.roles)).has(role))
+        }
+    }
+})
+
 Alpine.start();
