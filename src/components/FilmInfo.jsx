90a30d8780b5c16bf3b3e73b547555f63941ed8b
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2024-05-27 10:06:37 +0530

    add filminfo comp and use it

---
+++ src/components/FilmInfo.jsx
@@ -0,0 +1,38 @@
+import Link from "./Link"
+export default function FilmInfo({ work }) {
+    let info = work.info
+    info.year = work.year
+
+    return <div className="bg-gray-50 max-w-prose mx-auto">
+        {info.thumbnail &&<img className="aspect-video w-full object-cover" src={info.thumbnail} alt={work.name} />}
+        {!info.thumbnail &&<div className="aspect-video w-full bg-gray-200 flex items-center justify-center"  >{work.name}</div>}
+        <h2 className="font-bold text-lg px-2 mt-3">{work.name}</h2>
+        <div className="px-2">
+            <span className="">{info.year}</span>
+            {info.duration && <span className=""> &bull; {info.duration}</span>}
+            {info.language && <span className=""> &bull; {info.language}</span>}
+        </div>
+        <div className="mt-3 px-2 pb-2 flex gap-4 flex-wrap">
+            {info.director && <div><small className="font-bold">Director</small><div className="">{info.director}</div></div>}
+            {info.dop && <div><small className="font-bold">DoP</small><div className="">{info.dop}</div></div>}
+            {info.producer && <div><small className="font-bold">Producer</small><div className="">{info.producer}</div></div>}
+            {/* <table>
+                <tbody>
+                    <tr>
+                        <th>Director</th>
+                        <td>{info.director}</td>
+                    </tr>
+                    <tr>
+                        <th>DoP</th>
+                        <td>{info.dop}</td>
+                    </tr>
+                </tbody>
+            </table> */}
+        </div>
+        {info.links?.length && <div className="mt-3 px-2 pb-2 flex">
+            {info.links.map((link) => {
+                return <Link link={link} />
+            })}
+        </div>}
+    </div>
+}
--- src/components/WorkInfo.jsx
+++ src/components/WorkInfo.jsx
@@ -1,4 +1,5 @@
 import Link from "./Link";
+import FilmInfo from "./FilmInfo";
 
 export default function WorkInfo({work}) {
     // console.log(work);
@@ -7,60 +8,67 @@ export default function WorkInfo({work}) {
         enumData = Object.entries(work.info);
     }
     // console.log(enumData);
-    return <div className="flex shadow rounded bg-gray-50 hover:bg-gray-100 p-1.5" >
+    return <>
         {
-            work.info?.thumbnail&&<img className="h-20 rounded aspect-video object-cover" src={work.info.thumbnail} alt={work.name} />
+            work.info?.type == 'short-film' && <FilmInfo work={work}></FilmInfo>
         }
         {
-            !work.info?.thumbnail&& <div className="h-20 rounded aspect-video bg-gray-200"></div>
-        }
+            work.info?.type != 'short-film' && <div className="flex shadow rounded bg-gray-50 hover:bg-gray-100 p-1.5" >
+            {
+                work.info?.thumbnail&&<img className="h-20 rounded aspect-video object-cover" src={work.info.thumbnail} alt={work.name} />
+            }
+            {
+                !work.info?.thumbnail&& <div className="h-20 rounded aspect-video bg-gray-200"></div>
+            }
 
-    
-        {
-            <div className="w-full text-left px-4">
-                <h2 className="text-center bg-gray-200 px-2">{work.name}</h2>
-                {
-                    enumData && <table className="w-full">
-                    <tbody>
-                        {
-                            
-                            enumData.map(([key, value]) => {
-                              
-                                return <tr key={key}><th className="px-2" scope="row">{key}</th>
-                                    <td className="px-2" >
-                                        {
-                                            (key == 'link') && (value.platform == 'youtube') && <Link link={value} />
-                                        }
-                                        {
-                                            (key == 'link') &&(!value.platform) && <a href={value.url} target="_blank">{value.type}</a>
+        
+            {
+                <div className="w-full text-left px-4">
+                    <h2 className="text-center bg-gray-200 px-2">{work.name}</h2>
+                    {
+                        enumData && <table className="w-full">
+                        <tbody>
+                            {
+                                
+                                enumData.map(([key, value]) => {
+                                
+                                    return <tr key={key}><th className="px-2" scope="row">{key}</th>
+                                        <td className="px-2" >
+                                            {
+                                                (key == 'link') && (value.platform == 'youtube') && <Link link={value} />
+                                            }
+                                            {
+                                                (key == 'link') &&(!value.platform) && <a href={value.url} target="_blank">{value.type}</a>
+                                                
+                                            }
+                                            {
+                                                (key != 'link') &&
+                                                <span>{value}</span>
+                                            }
                                             
-                                        }
-                                        {
-                                            (key != 'link') &&
-                                            <span>{value}</span>
-                                        }
-                                        
-                                    </td>
-                                </tr>
-                            } )
-                        }
-                            {/* <tr><th className="px-2" scope="row">Director</th><td className="px-2" >{ work.data.director}</td></tr>
-                            <tr><th className="px-2" scope="row">DoP</th><td className="px-2" >{work.data.dop}</td></tr>
-                            <tr><th className="px-2" scope="row">Duration</th><td className="px-2" >{  work.data.duration}</td></tr> */}
-                    </tbody>
-                
-                </table>
-                }
-                
-                {
-                work.info?.link && 
-                    <a className="block text-gray-400 hover:text-gray-600" href={work.info.link.url} target="_blank">{work.info.link.type }</a>
-                }
+                                        </td>
+                                    </tr>
+                                } )
+                            }
+                                {/* <tr><th className="px-2" scope="row">Director</th><td className="px-2" >{ work.data.director}</td></tr>
+                                <tr><th className="px-2" scope="row">DoP</th><td className="px-2" >{work.data.dop}</td></tr>
+                                <tr><th className="px-2" scope="row">Duration</th><td className="px-2" >{  work.data.duration}</td></tr> */}
+                        </tbody>
+                    
+                    </table>
+                    }
+                    
+                    {
+                    work.info?.link && 
+                        <a className="block text-gray-400 hover:text-gray-600" href={work.info.link.url} target="_blank">{work.info.link.type }</a>
+                    }
+            </div>
+            }
+            
+
         </div>
         }
-        
-
-    </div>
+    </>
 
     
 }
