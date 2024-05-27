1c60b9b0a82d1593f519a690c6b5de6c06a0c2a8
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2024-05-27 10:06:46 +0530

    add icons to link btn

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,6 +1,18 @@
 export default function Link({ link }) {
 
-    return <a href={link.url} target="_blank" rel="noreferrer">
-        {link.type} - {link.platform} <i className="fas fa-external-link-alt"></i>
+    return <a className="bg-gray-200 hover:bg-gray-100 rounded px-2 py-0.5 inline-block" href={link.url} target="_blank" rel="noreferrer">
+        {link.platform == 'youtube' &&
+            <svg className="w-4 h-4 inline align-middle -mt-1" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 576 512">
+                {/* <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+                <path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" />
+            </svg>
+        }
+        &nbsp;
+        {link.type}
+        &nbsp;
+        <svg className="w-3 h-3 inline align-middle  -mt-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            {/* <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+            <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" />
+        </svg>
     </a>
 }
