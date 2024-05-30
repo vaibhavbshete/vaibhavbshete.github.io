a8731bac1f966b95fba7c87ba2049ffd86f48c14
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2024-05-30 09:53:25 +0530

    link and thumb in workinfo - layout

--- src/components/WorkInfo.jsx
+++ src/components/WorkInfo.jsx
@@ -13,7 +13,7 @@ export default function WorkInfo({work}) {
             work.info?.type == 'short-film' && <FilmInfo work={work}></FilmInfo>
         }
         {
-            work.info?.type != 'short-film' && <div className="flex flex-wrap sm:flex-nowrap bg-gray-50 hover:bg-gray-100 " >
+            work.info?.type != 'short-film' && <div className="flex flex-wrap sm:flex-nowrap bg-gray-50 " >
             {
                 work.info?.thumbnail&&<img className="w-full sm:w-auto sm:h-40 aspect-video object-cover" src={work.info.thumbnail} alt={work.name} />
             }
@@ -24,23 +24,23 @@ export default function WorkInfo({work}) {
         
             {
                 <div className="w-full text-left px-2">
-                    <h2 className="text-center font-semibold px-2">{work.name}</h2>
+                    <h2 className="text-center font-semibold px-2 py-1">{work.name}</h2>
                     {
-                        enumData && <table className="w-full">
+                        enumData && <table className="w-full mt-1">
                         <tbody>
                             {
                                 
                                 enumData.map(([key, value]) => {
                                 
-                                    return key !='type' && <tr key={key}><th className="px-2" scope="row">{key}</th>
+                                    return key !='type' && key!='link' && key!='thumbnail' && <tr key={key}><th className="px-2" scope="row">{key}</th>
                                         <td className="px-2" >
                                             {
-                                                (key == 'link') && (value.platform == 'youtube') && <Link link={value} />
+                                                (key == 'link') && <Link link={value} />
                                             }
-                                            {
+                                            {/* {
                                                 (key == 'link') &&(!value.platform) && <a href={value.url} target="_blank">{value.type}</a>
                                                 
-                                            }
+                                            } */}
                                             {
                                                 (key != 'link') &&
                                                 <span>{value}</span>
@@ -59,8 +59,8 @@ export default function WorkInfo({work}) {
                     }
                     
                     {
-                    work.info?.link && 
-                        <a className="block text-gray-400 hover:text-gray-600" href={work.info.link.url} target="_blank">{work.info.link.type }</a>
+                    work.info?.link && <Link link={work.info.link} className={"mx-2 mt-3"} />
+                        // <a className="block text-gray-400 hover:text-gray-600" href={work.info.link.url} target="_blank">{work.info.link.type }</a>
                     }
             </div>
             }
