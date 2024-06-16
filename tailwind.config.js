13c88ae95b7ef61b861ad2fb8a1e25380f966fcd
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2024-06-16 14:37:36 +0530

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './index.html',
     './404.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {},
+  future: {
+    hoverOnlyWhenSupported: true,
   },
-  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        'media-brand': 'rgb(var(--media-brand) / <alpha-value>)',
+        'media-focus': 'rgb(var(--media-focus) / <alpha-value>)',
+      },
+    },
+  },
+  plugins: [
+    require('tailwindcss-animate'),
+    require('@vidstack/react/tailwind.cjs')({
+      prefix: 'media',
+    }),
+    customVariants,
+  ],
+};
+
+function customVariants({ addVariant, matchVariant }) {
+  // Strict version of `.group` to help with nesting.
+  matchVariant('parent-data', (value) => `.parent[data-${value}] > &`);
+
+  addVariant('hocus', ['&:hover', '&:focus-visible']);
+  addVariant('group-hocus', ['.group:hover &', '.group:focus-visible &']);
 }
