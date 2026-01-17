ec98f5b4375f4b99088ebb9613ebdc03d7783156
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2026-01-17 19:12:20 +0530

    Spacing, layout and colour adjust ments in WorkInfo
    - button attached to bottom
    - card padding
    - image border
    - title bottom border

--- src/components/WorkInfo.jsx
+++ src/components/WorkInfo.jsx
@@ -13,16 +13,17 @@ export default function WorkInfo({work}) {
             work.info?.type == 'film' && <FilmInfo work={work}></FilmInfo>
         }
         {
-            work.info?.type != 'film' && <div className="flex flex-wrap sm:flex-nowrap bg-gray-50 " >
+            work.info?.type != 'film' && <div className="flex flex-wrap sm:flex-nowrap bg-gray-50 p-2" >
             {
-                work.info?.thumbnail&&<img className="w-full sm:w-auto sm:h-40 aspect-video object-cover" src={work.info.thumbnail} alt={work.name} />
+                work.info?.thumbnail&&<img className="w-full sm:w-auto sm:h-40 aspect-video object-cover border-gray-200 border-2" src={work.info.thumbnail} alt={work.name} />
             }
             {
                 !work.info?.thumbnail&& <div className="w-full sm:w-auto sm:h-40 aspect-video bg-gray-200"></div>
             }
             {
-                <div className="w-full text-left px-2">
-                    <h2 className="text-center font-semibold px-2 py-1 bg-gray-200">{work.name}</h2>
+                <div className="w-full text-left px-2 flex flex-col">
+                        <h2 className="text-center font-semibold px-2 py-1 border-b-2 border-b-gray-300">{work.name}</h2>
+                        <div className="flex flex-col justify-between flex-grow">
                     {
                         enumData && <table className="w-full mt-1">
                         <tbody>
@@ -42,13 +43,15 @@ export default function WorkInfo({work}) {
                                 } )
                             }
                         </tbody>
-                        </table>
+                            </table>
+                                
                     }
                     
                     {
-                    work.info?.link && <Link link={work.info.link} className={"mx-2 mt-7"} />
+                    work.info?.link && <Link link={work.info.link} className={"mx-2 w-max"} />
                         // <a className="block text-gray-400 hover:text-gray-600" href={work.info.link.url} target="_blank">{work.info.link.type }</a>
-                    }
+                                }
+                                </div>
             </div>
             }
             
