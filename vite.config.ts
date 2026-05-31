8d5f7efa46e987faaee7fd6ad4a28ee0685ddf31
vaibhavbshete <30766384+vaibhavbshete@users.noreply.github.com>
2026-06-01 02:19:40 +0530

    do not input plane in vite config rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import react  from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     build: {
         rollupOptions: {
             input: {
                 main: resolve (__dirname, 'index.html'),
-                plane: resolve (__dirname, 'plane-scroll.html'),
+                // plane: resolve (__dirname, 'plane-scroll.html'),
                 err404: resolve (__dirname, '404.html'),
             }
         },
